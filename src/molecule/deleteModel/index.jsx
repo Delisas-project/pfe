98f3cd1a1288b611b7aa8879c99a20209bab91ba
BelguesmiSakr <belguesmi_sakr@yahoo.com>
2022-04-21 01:11:37 +0100

    delete button design

--- src/molecule/deleteModel/index.jsx
+++ src/molecule/deleteModel/index.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import "./style.css";
 
-function DeleteModel() {
+function DeleteModel({ setshowDeleteModel }) {
 	return (
 		<div className='modelBackGround flex'>
-			<div className='model flexCol'>
+			<div className='deleteModel'>
 				<div className='deleteLabel flexStart'>Supprimer Personnel</div>
 				<div className='deleteText flexStart'>
 					Voulez vous vraiment supprimer ce personnel ?
 				</div>
 				<div className='buttons flexEnd'>
-					<button className='cancel'>Annuler</button>
+					<button
+						className='cancel'
+						onClick={() => setshowDeleteModel(false)}
+					>
+						Annuler
+					</button>
 					<button className='delete'>Supprimer</button>
 				</div>
 			</div>

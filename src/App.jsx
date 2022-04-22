4a78be51c92b69a40ba9c5ecf979065e988a1814
aladin-jridi <aladinjridi007@gmail.com>
2022-04-22 19:45:42 +0100

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
-import BackOfficeInterface from "./template/backOfficeInterface";
+// import BackOfficeInterface from "./template/backOfficeInterface";
+
+import Login from "./organism/login";
 
 function App() {
 	return (
 		<div className='app'>
-			<BackOfficeInterface />
+			{/* <BackOfficeInterface /> */}
+			<Login/>
 		</div>
 	);
 }

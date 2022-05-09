a84c146344b41ed6ab6619ae6e40d13ff347a27b
BelguesmiSakr <belguesmi_sakr@yahoo.com>
2022-05-10 00:08:15 +0100

    merge 7sak

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "./App.css";
-// import BackOfficeInterface from "./template/backOfficeInterface";
-
-import Login from "./organism/login";
+import BackOfficeInterface from "./template/backOfficeInterface";
+// import Login from "./organism/login";
 
 function App() {
 	return (
 		<div className='app'>
-			{/* <BackOfficeInterface /> */}
-			<Login/>
+			<BackOfficeInterface />
+			{/* <Login/> */}
 		</div>
 	);
 }
